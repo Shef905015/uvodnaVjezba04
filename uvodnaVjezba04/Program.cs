a17693a3ca58265581d96adf2ab617d3664d11c7
Non't <90344104+Shef905015@users.noreply.github.com>
2023-09-24 12:15:31 +0200

    Add files via upload

---
+++ uvodnaVjezba04/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uvodnaVjezba04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int sljedeci = 0;
+            int rezultat = 0;
+
+            Console.Write("Unesite broj fibbonacijevog niza: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            sljedeci.Add(1);
+            for (int i = 0; i < x; i++)
+            {
+
+                rezultat = rezultat + sljedeci;
+
+
+            }
+
+            Console.WriteLine(x + ". broj fibbonacijevog niza je: " + rezultat);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
